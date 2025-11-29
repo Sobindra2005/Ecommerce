fd16ae53484c7f1040a96c3069351887ed72316e
sobindra2005 <sobhindra2005@gmail.com>
2025-11-29 12:34:23 +0545

    fix: add unused variable suppression in error handler middleware

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -175,6 +175,7 @@ const errorHandler = (
   err: Error & Partial<AppError> & Partial<MongoError>,
   _req: Request,
   res: Response,
+  //eslint-disable-next-line @typescript-eslint/no-unused-vars
   _next: NextFunction
 ): void => {
   const error = {
